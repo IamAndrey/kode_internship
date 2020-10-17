c32ede97f32bc25f20108c1a70fd39a880adfa8c
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-17 22:32:12 +0200

    Added basic style

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Header from "../Header/Header";
+import LoginPage from "../../pages/LoginPage";
+import LoginVerificationPage from "../../pages/LoginVerificationPage";
+import PokemonsPage from "../../pages/PokemonsPage";
+import Pokemon from "../Pokemon/Pokemon";
+import './app.css'
+
+function App() {
+  return (
+      <BrowserRouter>
+          <div className='app'>
+              <header>
+                  <Header />
+              </header>
+              <section>
+                  <div className='container'>
+                      <Switch>
+                          <Route path='/login' component={LoginPage} exact/>
+                          <Route path='/login-verification' component={LoginVerificationPage} exact/>
+                          <Route path='/pokemons' component={PokemonsPage} exact/>
+                          <Route path='/pokemon' component={Pokemon} exact/>
+                      </Switch>
+                  </div>
+              </section>
+          </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
