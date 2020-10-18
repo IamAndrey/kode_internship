798a51fb59d31858cdb04afad5da71548a0909dc
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-18 22:22:48 +0200

    Updated authReducer

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,9 +1,20 @@
+import {LOGIN, LOGOUT} from "../types";
+
 const initialState = {
     authenticated: false
 }
 
 export default function (state = initialState, action) {
     switch (action.type) {
+        case LOGIN:
+            return {
+                ...state,
+                authenticated: true
+            }
+        case LOGOUT:
+            return {
+                authenticated: false
+            }
         default:
             return initialState
     }
