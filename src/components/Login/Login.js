40634f1bb3f2c6ab14d961900c7e128b9fae2e24
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-18 23:25:39 +0200

    Added login-logic to Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,36 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import {withRouter} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
+import {login as entry} from "../../redux/actions/authActions";
 import './login.scss'
+import lookPass from '../../assets/svg/look-pass.svg'
 
-const Login = () => {
+const Login = ({history}) => {
+
+    const _USER_LOGIN = 'test';
+    const _USER_PASSWORD = '123456';
 
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
+
+    const authenticated = useSelector(({auth: {authenticated}}) => authenticated)
+    const dispatch = useDispatch()
+
+    const handleLogin = (event) => {
+        event.preventDefault()
+        setTimeout(() => {
+            if (_USER_LOGIN === login && _USER_PASSWORD === password) {
+                entry()(dispatch)
+                history.push('/login-verification')
+            }
+        }, 2000)
+    }
 
     return (
         <div className="login-page container-login">
             <h2>Вход в профиль</h2>
             <div className="input-item">
-                <form onSubmit={() => {}}>
+                <form onSubmit={handleLogin}>
                     <div className="content-input">
                         <div className="input-wrapper">
                             <label className="headline">Логин</label>
@@ -26,9 +46,9 @@ const Login = () => {
                                    name="password" placeholder="Введите пароль"
                                    type="password" required minLength={6}/>
 
-                            <button onClick={() => {
-                            }} type="button" className="look-pass"
+                            <button onClick={() => {}} type="button" className="look-pass"
                                     data-for="look-pass" data-delay-show="200" data-tip="Показать пароль">
+                                <img src={lookPass} alt='icon' />
                             </button>
                         </div>
                     </div>
@@ -40,4 +60,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default withRouter(Login)
