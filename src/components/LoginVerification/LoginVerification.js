771476311f5c5369d93ac3ac562ed2406abda8e8
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-19 20:48:40 +0200

    Updated LoginVerification component

--- src/components/LoginVerification/LoginVerification.js
+++ src/components/LoginVerification/LoginVerification.js
@@ -1,10 +1,33 @@
-import React, {useState} from "react";
+import React, {Fragment, useState} from "react";
+import {withRouter} from "react-router-dom";
+import './style.scss'
 
 const LoginVerification = () => {
 
+    const [code, setCode] = useState('')
+
     return (
-        <div className='login-verification'>LoginVerification</div>
+        <Fragment>
+            <div className="login-verification-page container-login-verification">
+                <h2>Подтвердите вход</h2>
+                <div className="input-item">
+                    <form onSubmit={() => {}}>
+                        <div className="content-input">
+                            <div className="input-wrapper">
+                                <label className="headline">Код</label>
+                                <input className="code input-info" onChange={(event) => setCode(event.target.value)}
+                                       value={code}
+                                       name="code" placeholder="Введите код из смс"
+                                       type="code" required minLength={6}/>
+                            </div>
+                        </div>
+                        <button type="submit" >Подтвердить</button>
+                    </form>
+                </div>
+            </div>
+
+        </Fragment>
     )
 }
 
-export default LoginVerification
+export default withRouter(LoginVerification)
