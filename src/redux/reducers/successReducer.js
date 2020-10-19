6986a8e4898607e9858b0cf1c1c9268b6694b2dc
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-19 22:03:45 +0200

    Added success types, modify successReducer

--- src/redux/reducers/successReducer.js
+++ src/redux/reducers/successReducer.js
@@ -1,3 +1,4 @@
+import {CLEAR_SUCCESS, SET_SUCCESS} from "../types";
 
 const initialState = {
     success: {
@@ -7,7 +8,16 @@ const initialState = {
 
 export default function (state = initialState, action) {
     switch (action.type) {
-
+        case SET_SUCCESS:
+            return {
+                ...state,
+                success: {
+                    ...state.success,
+                    message: action.payload
+                }
+            }
+        case CLEAR_SUCCESS:
+            return initialState
         default:
             return state
     }
--- src/redux/types.js
+++ src/redux/types.js
@@ -11,3 +11,7 @@ export const LOADING_STOP = 'LOADING_STOP'
 // error
 export const SET_ERROR = 'SET_ERROR'
 export const CLEAR_ERROR = 'CLEAR_ERROR'
+
+// success
+export const SET_SUCCESS = 'SET_SUCCESS'
+export const CLEAR_SUCCESS = 'CLEAR_SUCCESS'
