bbb94727eeb1dcc7ed607e6a59f775198a08d69e
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-21 17:45:07 +0200

    Modified Filter style.cscc

--- src/components/Filter/Flter.js
+++ src/components/Filter/Flter.js
@@ -1,9 +1,33 @@
 import React from "react";
 import './style.scss'
+import arrow from '../../assets/svg/arrow.svg'
+
 
 const Filter = () => {
     return (
-        <div className='container-filter'>Filter</div>
+        <div className='container-filter'>
+            <h4>Выбор категорий</h4>
+            <div className="filter-content">
+                <div className='filter-row'>
+                    <select onChange={() => {}} defaultValue='default'>
+                        <option value="default" disabled>Тип:</option>
+                        <option value="skills">Навыкам</option>
+                        <option value="technologies">Технологиям</option>
+                    </select>
+                    <img src={arrow} alt='arrow'/>
+                </div>
+            </div>
+            <div className="filter-content">
+                <div className='filter-row'>
+                    <select onChange={() => {}} defaultValue='default'>
+                        <option value="default" disabled>Подтип:</option>
+                        <option value="skills">Навыкам</option>
+                        <option value="technologies">Технологиям</option>
+                    </select>
+                    <img src={arrow} alt='arrow'/>
+                </div>
+            </div>
+        </div>
     )
 }
 
