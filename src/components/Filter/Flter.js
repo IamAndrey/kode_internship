7b5f916dff51871ef27946851cd9f01565548b79
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-21 19:43:44 +0200

    Modified Filter options

--- src/components/Filter/Flter.js
+++ src/components/Filter/Flter.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {getTypes} from "../../redux/actions/typeActions";
+import {getSubtypes} from "../../redux/actions/subtypeActions";
 import './style.scss'
 import arrow from '../../assets/svg/arrow.svg'
+import Option from "./Option";
 
 
 const Filter = () => {
+
+    const {types, subtypes} = useSelector(
+        ({type: {types}, subtype: {subtypes}}) =>
+            ({types, subtypes}))
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        getTypes()(dispatch)
+        getSubtypes()(dispatch)
+    }, [])
+
+    const typeOptions = types.map(type => <Option value={type} key={type} />)
+    const subtypeOptions = subtypes.map(subtype => <Option value={subtype} key={subtype} />)
+
     return (
         <div className='container-filter'>
             <h4>Выбор категорий</h4>
             <div className="filter-content">
                 <div className='filter-row'>
                     <select onChange={() => {}} defaultValue='default'>
-                        <option value="default" disabled>Тип:</option>
-                        <option value="skills">Навыкам</option>
-                        <option value="technologies">Технологиям</option>
+                        <option value="default" disabled>Type:</option>
+                        {typeOptions}
                     </select>
                     <img src={arrow} alt='arrow'/>
                 </div>
@@ -20,9 +37,8 @@ const Filter = () => {
             <div className="filter-content">
                 <div className='filter-row'>
                     <select onChange={() => {}} defaultValue='default'>
-                        <option value="default" disabled>Подтип:</option>
-                        <option value="skills">Навыкам</option>
-                        <option value="technologies">Технологиям</option>
+                        <option value="default" disabled>Subtype:</option>
+                        {subtypeOptions}
                     </select>
                     <img src={arrow} alt='arrow'/>
                 </div>
---
+++ src/components/Filter/Option.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Option = ({value}) => {
+
+    return (
+        <option value={value}>{value}</option>
+    )
+}
+
+export default Option
