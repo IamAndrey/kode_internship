ffce5738bab7d2fea1daf89e1b68e203ea4ac727
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-21 19:15:23 +0200

    Modified subtypes action

--- src/redux/actions/subtypeActions.js
+++ src/redux/actions/subtypeActions.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+import {SET_SUBTYPES} from "../types";
 
+export const getSubtypes = () => dispatch => {
+    axios.get('https://api.pokemontcg.io/v1/subtypes')
+        .then(res => {
+            dispatch({type: SET_SUBTYPES, payload: res.data.subtypes})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
