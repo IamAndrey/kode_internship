460c33318f43ec905c49a5ef597ec98ae4bf986a
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-22 17:36:47 +0200

    Modified Card component

--- src/components/Pokemons/Card.js
+++ src/components/Pokemons/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({card}) => {
     return (
         // eslint-disable-next-line jsx-a11y/anchor-is-valid
-        <a href='#'>
+        <a href={`/pokemons/${card.id}`}>
             <img src={card.imageUrl} alt='Card' />
         </a>
     )
