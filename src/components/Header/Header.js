c0ff1d623ea38f415839c377df8919712bde7606
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-23 23:28:29 +0200

    Modified Header, added goBack

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from "react";
+import {withRouter} from 'react-router-dom'
 import './style.scss'
 import {Link} from "react-router-dom";
 import arrowBack from '../../assets/svg/arrow-back.svg'
 
-const Header = () => {
+const Header = ({history}) => {
 
     return (
         <div className='container-header'>
             <div className='header-left'>
                 <img src={arrowBack} alt='arrow-back'/>
                 <div className="link">
-                    <Link to="#" className="back p2">назад</Link>
+                    <Link to="#" className="back p2" onClick={() => history.goBack()}>назад</Link>
                 </div>
             </div>
             <div className='header-right'>
@@ -22,4 +23,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default withRouter(Header)
