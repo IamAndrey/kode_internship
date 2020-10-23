5f039efe663a7b96c5d1302ae27e212af25a2f4b
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-23 22:45:05 +0200

    Added CardSkeleton component

---
+++ src/components/Loader/Skeleton/CardSkeleton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.scss'
+
+const CardSkeleton = () => {
+
+    return (
+        <div className='card-skeleton'></div>
+    )
+}
+
+export default CardSkeleton
