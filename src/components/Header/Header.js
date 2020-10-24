d592e72410cb6e2b3fc5b727f086e0b8f28b2621
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-24 08:34:20 +0200

    Hide Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {useDispatch} from "react-redux";
+import React, {Fragment} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import {logout} from "../../redux/actions/authActions";
 import {withRouter} from 'react-router-dom'
 import './style.scss'
@@ -8,22 +8,26 @@ import arrowBack from '../../assets/svg/arrow-back.svg'
 
 const Header = ({history}) => {
 
+    const {authenticated, verified} = useSelector(({auth: {authenticated, verified}}) => ({authenticated, verified}))
     const dispatch = useDispatch()
 
     return (
-        <div className='container-header'>
-            <div className='header-left'>
-                <img src={arrowBack} alt='arrow-back'/>
-                <div className="link">
-                    <Link to="#" className="back p2" onClick={() => history.goBack()}>назад</Link>
-                </div>
-            </div>
-            <div className='header-right'>
-                <div className="link" >
-                    <Link to="/login" className="exit p2" onClick={() => logout()(dispatch)}>Выйти</Link>
-                </div>
-            </div>
-        </div>
+        <Fragment>
+            {authenticated && verified ?
+                <div className='container-header'>
+                    <div className='header-left'>
+                        <img src={arrowBack} alt='arrow-back'/>
+                        <div className="link">
+                            <Link to="#" className="back p2" onClick={() => history.goBack()}>назад</Link>
+                        </div>
+                    </div>
+                    <div className='header-right'>
+                        <div className="link" >
+                            <Link to="/login" className="exit p2" onClick={() => logout()(dispatch)}>Выйти</Link>
+                        </div>
+                    </div>
+                </div> : null}
+        </Fragment>
     )
 }
 
