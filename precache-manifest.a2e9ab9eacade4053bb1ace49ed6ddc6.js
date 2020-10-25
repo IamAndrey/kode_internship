6eaf30f38d6af11428a209d3ca9b2d21735d3e8e
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-25 21:56:07 +0200

    Added new branch "gh-pages"

---
+++ precache-manifest.a2e9ab9eacade4053bb1ace49ed6ddc6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3919353ecef2579d138694cfa1a6c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1640afc1cf20419b07c2",
+    "url": "/static/css/main.265d35ea.chunk.css"
+  },
+  {
+    "revision": "51ce015f484525f8ef81",
+    "url": "/static/js/2.4b28e856.chunk.js"
+  },
+  {
+    "revision": "c31ad099280d8d79fdabf1f60558c035",
+    "url": "/static/js/2.4b28e856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1640afc1cf20419b07c2",
+    "url": "/static/js/main.b7f9ca55.chunk.js"
+  },
+  {
+    "revision": "e2fee617b0cce52d38a2",
+    "url": "/static/js/runtime-main.fde3a231.js"
+  },
+  {
+    "revision": "c891f8648a8126915649bdc1dec8e729",
+    "url": "/static/media/Magnifier.c891f864.svg"
+  },
+  {
+    "revision": "c90677b51b46075479c08d267bf4543a",
+    "url": "/static/media/account.c90677b5.svg"
+  },
+  {
+    "revision": "254bfcc1f23d118c32a75a2f8d60f235",
+    "url": "/static/media/arrow-back.254bfcc1.svg"
+  },
+  {
+    "revision": "404caa1b9cf0e2e99a68431db4195db4",
+    "url": "/static/media/arrow-down.404caa1b.svg"
+  },
+  {
+    "revision": "3876df7b7b1eb7d757e727de5aa4d250",
+    "url": "/static/media/check.3876df7b.svg"
+  },
+  {
+    "revision": "b2f4784114a7fcdf0ab168f687ab3c8c",
+    "url": "/static/media/close.b2f47841.svg"
+  },
+  {
+    "revision": "6dc9730abe31bf2be7129fe43ca772aa",
+    "url": "/static/media/error.6dc9730a.svg"
+  },
+  {
+    "revision": "dd06ad6458fda3a1b05f4ba1cb94bdea",
+    "url": "/static/media/look-pass.dd06ad64.svg"
+  },
+  {
+    "revision": "d9fe0fec818640a2e1912a0630b3e021",
+    "url": "/static/media/password.d9fe0fec.svg"
+  }
+]);
