be29524c9b92b009400e505ff4fd03a4a1f0ed10
IamAndrey <a.andrey.victorovich@gmail.com>
2020-10-25 22:56:01 +0200

    Updates

---
+++ kode_internship/precache-manifest.3b2eb6afe5615fce7bf561bea1248c07.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b4cd099af1beca70c91f3510e01b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd177633e2c4231fdc9d",
+    "url": "/static/css/main.08323ba1.chunk.css"
+  },
+  {
+    "revision": "96bbb79d7c8f520dfc23",
+    "url": "/static/js/2.d6c5f348.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d6c5f348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd177633e2c4231fdc9d",
+    "url": "/static/js/main.65fa7137.chunk.js"
+  },
+  {
+    "revision": "e2fee617b0cce52d38a2",
+    "url": "/static/js/runtime-main.fde3a231.js"
+  },
+  {
+    "revision": "c891f8648a8126915649bdc1dec8e729",
+    "url": "/static/media/Magnifier.c891f864.svg"
+  },
+  {
+    "revision": "c90677b51b46075479c08d267bf4543a",
+    "url": "/static/media/account.c90677b5.svg"
+  },
+  {
+    "revision": "254bfcc1f23d118c32a75a2f8d60f235",
+    "url": "/static/media/arrow-back.254bfcc1.svg"
+  },
+  {
+    "revision": "404caa1b9cf0e2e99a68431db4195db4",
+    "url": "/static/media/arrow-down.404caa1b.svg"
+  },
+  {
+    "revision": "3876df7b7b1eb7d757e727de5aa4d250",
+    "url": "/static/media/check.3876df7b.svg"
+  },
+  {
+    "revision": "b2f4784114a7fcdf0ab168f687ab3c8c",
+    "url": "/static/media/close.b2f47841.svg"
+  },
+  {
+    "revision": "6dc9730abe31bf2be7129fe43ca772aa",
+    "url": "/static/media/error.6dc9730a.svg"
+  },
+  {
+    "revision": "dd06ad6458fda3a1b05f4ba1cb94bdea",
+    "url": "/static/media/look-pass.dd06ad64.svg"
+  },
+  {
+    "revision": "d9fe0fec818640a2e1912a0630b3e021",
+    "url": "/static/media/password.d9fe0fec.svg"
+  }
+]);
